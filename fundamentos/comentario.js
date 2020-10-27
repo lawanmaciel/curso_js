dd3a1b1af982c357130f14ac1f501b3a34e3e2d4
Lawan Maciel <irolawan33@gmail.com>
2020-10-27 00:50:19 -0300

    Subindo dados de casa

--- fundamentos/comentario.js
+++ fundamentos/comentario.js
@@ -1,2 +1,15 @@
 // Comentário de uma linha
 console.log('Linha 1')
+
+/*
+    Comentário de
+    múltiplas linhas
+*/
+
+console.log('Linhas 2')
+
+/*
+*   Comentário de
+*   múltiplas linhas
+*/
+console.log('Linhas 3')
---
+++ fundamentos/strings.js
@@ -0,0 +1,5 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4))
+// console.log(escolas.charAt(5))
+// console.log(escola.charAt(3))
